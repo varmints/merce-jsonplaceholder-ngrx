f1538499080dab83958168fef1ac0baa8de1fd62
Kamil <you@example.com>
2021-10-05 21:01:24 +0200

    stylin post list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostsComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
